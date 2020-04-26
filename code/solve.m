21c66952f701d3b1290df732a0c76c58e34d8e19
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-04-26 10:13:06 -0400

    More cleanup and code simplifcation

--- code/solve.m
+++ code/solve.m
@@ -6,17 +6,11 @@ tic
 model1 = createpde;
 gd1 = importGeometry(model1,'../models/Part1.STL');
 generateMesh(model1);
-% pdegplot(model1,'FaceLabels','on','EdgeLabels','on','FaceAlpha',0.5);
-% hold on
+
 model2 = createpde;
 gd2 = importGeometry(model2,'../models/Part2.STL');
 generateMesh(model2);
-% pdegplot(model2,'FaceLabels','on','EdgeLabels','on','FaceAlpha',0.5);
-% figure;scatter3(model2.Mesh.Nodes(1,:), model2.Mesh.Nodes(2,:), model2.Mesh.Nodes(3,:),'b');
-% hold on
-% axis equal
-% scatter3(model1.Mesh.Nodes(1,:), model1.Mesh.Nodes(2,:), model1.Mesh.Nodes(3,:),'r');
-% hold off
+
 %% Finding a boundary
 % Part1
 Part1 = [model1.Mesh.Nodes(1,:)', model1.Mesh.Nodes(2,:)', model1.Mesh.Nodes(3,:)'];
@@ -26,6 +20,7 @@ F1 = faceNormal(TR1);
 P1 = incenter(TR1);
 origin = [38.6; 33.7960; 141.5215];
 P1 = P1 - origin';
+
 % Part2
 Part2 = [model2.Mesh.Nodes(1,:)', model2.Mesh.Nodes(2,:)', model2.Mesh.Nodes(3,:)'];
 k = boundary(Part2, 1);
@@ -33,18 +28,18 @@ TR2 = triangulation(k,Part2);
 F2 = faceNormal(TR2);
 P2 = incenter(TR2);
 P2 = P2 - origin';
-%% Chossing the vectors on surface
-pointMid1 = [33.09; 38.55; 136.3]-origin; vecMid1 = [0.8192; -1.979e-07; -0.5736];
-pointMid2 = [34.25; 36.75; 137.9]-origin; vecMid2 = [-0.8192; 6.604e-11; 0.5736];
-pointLeft1 = [15.38; 23.39; 129.7]-origin; vecLeft1 = [-0.766; -4.023e-07; -0.6428];
-pointLeft2 = [15.12; 22.76; 130]-origin; vecLeft2 = [0.766; -4.795e-08; 0.6428];
-pointRight1 = [62.68; 26.88; 87.83]-origin; vecRight1 = [-0.9397; 2.238e-08; -0.342];
-pointRight2 = [62.85; 26.13; 87.37]-origin; vecRight2 = [0.9397; -2.121e-08; 0.342];
-Point1 = [pointMid1, pointLeft1, pointRight1]; Vector1 = [vecMid1, vecLeft1, vecRight1];
-Point2 = [pointMid2, pointLeft2, pointRight2]; Vector2 = [vecMid2, vecLeft2, vecRight2];
-% Taking cross product of the vectors
-C = cross(Vector1, -Vector2);
-n = [norm(C(:,1)), norm(C(:,2)), norm(C(:,3))];
+
+%% Choosing the vectors on surface (hardcoded) and "breaking" bones.
+
+vecMid1 = [0.8192; -1.979e-07; -0.5736];
+vecMid2 = [-0.8192; 6.604e-11; 0.5736];
+vecLeft1 = [-0.766; -4.023e-07; -0.6428];
+vecLeft2 = [0.766; -4.795e-08; 0.6428];
+vecRight1 = [-0.9397; 2.238e-08; -0.342];
+vecRight2 = [0.9397; -2.121e-08; 0.342];
+Vector1 = [vecMid1, vecLeft1, vecRight1];
+Vector2 = [vecMid2, vecLeft2, vecRight2];
+
 % Rotating the vector around Z-axis
 angles = [5 10 30];
 rotateX = [1,      0,                 0;
@@ -61,10 +56,14 @@ rotate = rotateX * rotateY * rotateZ;
 rotateP1 = rotate * P1';
 Vector1_n = rotate * Vector1;
 
+%% A* Search
+
 % perform A* path search
-[success, nodes] = astar(Vector2, Vector1_n);
+[success, nodes] = custom.astar(Vector2, Vector1_n);
 
 %% Tracing back the path
+
+% goal node (should not be hardcoded...)
 vecMid2 = [-0.8192; 6.604e-11; 0.5736];
 vecLeft2 = [0.766; -4.795e-08; 0.6428];
 vecRight2 = [0.9397; -2.121e-08; 0.342];
@@ -72,7 +71,8 @@ if ~success
     error("Path search failed.");
 end
 
-[path, rRotateMatrix] = backtrack(nodes, [vecMid2, vecLeft2, vecRight2]);
+[path, rRotateMatrix] = custom.backtrack(nodes, [vecMid2, vecLeft2, vecRight2]);
+
 
 toc
 %% Display the path
@@ -131,7 +131,7 @@ plot_handle = patch('Faces', fv2.ConnectivityList, ...
                     'AmbientStrength', 0.15);
 
 i = 2;
-while (i < size(path,3))
+while (i ~= size(path,3))
     rotateP2 = rRotateMatrix{i} * P2';
     P2 = rotateP2';
 
@@ -139,7 +139,7 @@ while (i < size(path,3))
     fv2 = triangulation(fv2.ConnectivityList, rotateV2');
     set(plot_handle, 'Faces',fv2.ConnectivityList, 'Vertices',fv2.Points);
 
-    pause(0.05)
+    pause(0.01)
     i = i + 1;
 end
 hold off
